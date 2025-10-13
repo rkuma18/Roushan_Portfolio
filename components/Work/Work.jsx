be09e0bade813d91834d95622ff7c94b3cb2d7d2
rkuma18 <kumarroushan.18@gmail.com>
2025-10-13 12:08:35 +0530

    Update: 1 project added

--- components/Work/Work.jsx
+++ components/Work/Work.jsx
@@ -1,53 +1,59 @@
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import AnimatedText from "../AnimatedText";
-import WorkItem from "./WorkItems";
+import AnimatedText from '../AnimatedText';
+import WorkItem from './WorkItems';
 
 // sample data for projects with various categories
 const data = [
     {
-        href: "https://github.com/rkuma18/Resume_Filtering",
-        category: "ML",
-        img: "/assets/work/thumb-1.png",
-        title: "AI Resume Classifier",
+        href: 'https://github.com/rkuma18/financial-sms-ner-app',
+        category: 'GEN AI',
+        img: '/assets/work/thumb-8.png',
+        title: 'DistilBERT Fine-Tuned NER for SMS',
     },
     {
-        href: "https://github.com/rkuma18/Medical-Insurance-Cost-Prediction",
-        category: "ML",
-        img: "/assets/work/thumb-2.png",
-        title: "Medical Insurance Cost Prediction",
+        href: 'https://github.com/rkuma18/Resume_Filtering',
+        category: 'ML',
+        img: '/assets/work/thumb-1.png',
+        title: 'AI Resume Classifier',
     },
     {
-        href: "https://github.com/rkuma18/Medical-Insurance-Cost-Prediction-API",
-        category: "ML",
-        img: "/assets/work/thumb-4.png",
-        title: "Medical Insurance Cost Prediction API",
+        href: 'https://github.com/rkuma18/Medical-Insurance-Cost-Prediction',
+        category: 'ML',
+        img: '/assets/work/thumb-2.png',
+        title: 'Medical Insurance Cost Prediction',
     },
     {
-        href: "https://github.com/rkuma18/Chest_Cancer_Classification",
-        category: "DL",
-        img: "/assets/work/thumb-3.png",
-        title: "Chest Cancer Classification",
+        href: 'https://github.com/rkuma18/Medical-Insurance-Cost-Prediction-API',
+        category: 'ML',
+        img: '/assets/work/thumb-4.png',
+        title: 'Medical Insurance Cost Prediction API',
     },
     {
-        href: "https://huggingface.co/spaces/rkuma18/Currency_Converter_Chat",
-        category: "GEN AI",
-        img: "/assets/work/thumb-6.png",
-        title: "Currency Converter Agent",
+        href: 'https://github.com/rkuma18/Chest_Cancer_Classification',
+        category: 'DL',
+        img: '/assets/work/thumb-3.png',
+        title: 'Chest Cancer Classification',
     },
     {
-        href: "https://github.com/rkuma18/RAG_Powered_Chatbot",
-        category: "GEN AI",
-        img: "/assets/work/thumb-7.png",
-        title: "Conversational Chatbots Using RAG",
+        href: 'https://huggingface.co/spaces/rkuma18/Currency_Converter_Chat',
+        category: 'GEN AI',
+        img: '/assets/work/thumb-6.png',
+        title: 'Currency Converter Agent',
     },
     {
-        href: "https://github.com/rkuma18/Custom_MCP",
-        category: "GEN AI",
-        img: "/assets/work/thumb-5.png",
-        title: "AI Sticky Notes (MCP App)",
-    }
+        href: 'https://github.com/rkuma18/RAG_Powered_Chatbot',
+        category: 'GEN AI',
+        img: '/assets/work/thumb-7.png',
+        title: 'Conversational Chatbots Using RAG',
+    },
+    {
+        href: 'https://github.com/rkuma18/Custom_MCP',
+        category: 'GEN AI',
+        img: '/assets/work/thumb-5.png',
+        title: 'AI Sticky Notes (MCP App)',
+    },
 ];
 
 const Work = () => {
@@ -55,20 +61,16 @@ const Work = () => {
     const uniqueCategories = Array.from(new Set(data.map((item) => item.category)));
 
     // create tab data with 'all' category and unique categories from data
-    const tabData = [
-        { category: "all" },
-        ...uniqueCategories.map((category) => ({ category })),
-    ];
+    const tabData = [{ category: 'all' }, ...uniqueCategories.map((category) => ({ category }))];
 
     // state to manage the currently selected tab
-    const [tabValue, setTabValue] = useState("all");
+    const [tabValue, setTabValue] = useState('all');
     // number of items to show initially
     const [visibleItems, setVisibleItems] = useState(6);
 
     // filter work items based on the selected tab
-    const filterWork = tabValue === "all"
-        ? data
-        : data.filter((item) => item.category === tabValue);
+    const filterWork =
+        tabValue === 'all' ? data : data.filter((item) => item.category === tabValue);
 
     // handle loading more items
     const loadMoreItems = () => {
@@ -79,7 +81,12 @@ const Work = () => {
     return (
         <section className="pt-24 min-h-[1000px]" id="work">
             <div className="container mx-auto">
-                <Tabs defaultValue="all" className="w-full flex flex-col" value={tabValue} onValueChange={setTabValue}>
+                <Tabs
+                    defaultValue="all"
+                    className="w-full flex flex-col"
+                    value={tabValue}
+                    onValueChange={setTabValue}
+                >
                     <div className="flex flex-col xl:flex-row items-center xl:items-start xl:justify-between mb-[30px]">
                         <AnimatedText text="My Latest Work" textStyles="h2 mb-[30px] xl:mb-0" />
                         {/* render tab triggers */}
